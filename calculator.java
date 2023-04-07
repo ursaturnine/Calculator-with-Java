94a2b94ab80709f7f0bdb6fa9d527cc61e1a5c6c
Tyrah <tyrah96@gmail.com>
2023-04-07 13:56:37 -0700

    added conditional for sum

--- calculator.java
+++ calculator.java
@@ -7,13 +7,22 @@ class Calculator {
 
         System.out.print("Enter the first number ");
         double number1 = scanner.nextDouble();
+        scanner.nextLine();
 
         System.out.print("Enter the second number ");
         double number2 = scanner.nextDouble();
+        scanner.nextLine();
 
 
-        System.out.println(number1);
-        System.out.println(number2);
+        System.out.println("What operation do you want to perform? ");
+
+        String operation = scanner.nextLine();
+
+        if (operation.equals("sum")){
+            System.out.printf("%f + %f = %f", number1, number2, number1 + number2);
+        }
+
+        
 
 
 
